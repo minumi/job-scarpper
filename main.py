f37fec37a303eef4025088787a371b3e0ac30f13
minumi <kimkimee2@gmail.com>
2021-01-11 01:49:16 +0900

    Download jobs as csv file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from indeed import get_jobs as get_jobs_from_indeed
 from so import get_jobs as get_jobs_from_so
+from save import save_to_file
 
 indeed_jobs = get_jobs_from_indeed()
-so = get_jobs_from_so()
+so_jobs = get_jobs_from_so()
+jobs = indeed_jobs + so_jobs
+
+save_to_file(jobs)
 
